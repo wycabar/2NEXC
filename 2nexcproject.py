f8e796ba5b6f025ceb5042314e3a9ea7d79390b9
Wycabar <fnkyvilln@gmail.com>
2021-03-16 11:23:35 +1300

    SimpleProgram

--- 2nexcproject.py
+++ 2nexcproject.py
@@ -1,5 +1,19 @@
-canteen_menu = ["mince pie", "lolly cake", "cookie", "sandwich"]
+canteen_menu = ["-------", "MENU:", "mince pie", "lolly cake", "cookie", "sandwich", "-------"]
+my_cart = []
+continue_option = ""
+desired_item = ""
 
-print(canteen_menu)
-desired_item = input("what would you like ")
-print(desired_item)
+while continue_option != ("no"):
+  for item in canteen_menu:
+    print(item)
+  desired_item = input("what would you like to order?: ")
+  if desired_item in canteen_menu:
+    my_cart.append (desired_item)
+    continue_option = input("would you like to add another item? yes/ no: ")
+  else:
+    print("sorry, that's not on the menu")
+  
+
+print("your finished order:")
+for item in my_cart:
+  print(item)
